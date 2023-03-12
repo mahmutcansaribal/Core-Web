b3d356a6f0f121d90720fd29548be7828484b4fe
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2023-03-12 23:24:16 +0300

    EntityLayer Updated

---
+++ EntityLayer/Concrete/Expreince.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Expreince
+    {
+        public int ExpreinceID { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string IamgeUrl { get; set; }
+        public string Description { get; set; }
+
+    }
+}
